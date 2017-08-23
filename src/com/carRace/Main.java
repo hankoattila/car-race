f34d1d7bb668d8ac77a62a9500ae24f60a169e04
hankoattila <hattilla4@gmail.com>
2017-08-23 16:39:12 +0200

    createVehicles create 10-10 Car, Motorcycle, Truck

--- src/com/carRace/Main.java
+++ src/com/carRace/Main.java
@@ -1,12 +1,33 @@
 package com.carRace;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Main {
 
+    static List<Racer> racers = new ArrayList<>();
+
     public static void main(String[] args) {
+        createVehicles();
 
     }
-    void createVehicles(){
+    private static void createVehicles(){
+        racers.add(new Car("Cobra Exence"));
+        racers.add(new Car("Vertex Blaze"));
+        racers.add(new Car("Vindicator Freedom"));
+        racers.add(new Car("Whirlpool Blade"));
+        racers.add(new Car("Specter Fang"));
+        racers.add(new Car("Barrage Ethereal "));
+        racers.add(new Car("Magic Spirit"));
+        racers.add(new Car("Crux Blaze"));
+        racers.add(new Car("Hero Eagle"));
+        racers.add(new Car("Bullet Dynamo"));
 
+        for(int i =0; i<10;i++){
+            racers.add(new Motorcycle());
+            racers.add(new Truck());
+        }
     }
     void simulateRace(){
 
