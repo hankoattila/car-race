ba33024ba30ed9f5b8aef87b7217ab161f7f221f
hankoattila <hattilla4@gmail.com>
2017-08-24 08:56:14 +0200

    The painResult display a racers in 3 column

--- src/com/carRace/Main.java
+++ src/com/carRace/Main.java
@@ -1,23 +1,21 @@
 package com.carRace;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 
 public class Main {
 
-    static List<Racer> racers = new ArrayList<>();
-    static Random rnd = new Random();
+    private static List<Racer> racers = new ArrayList<>();
+    private static Random rnd = new Random();
+    private static boolean isRaining;
 
     public static void main(String[] args) {
-        createVehicles();
-        simulateRace();
-
-
+            createVehicles();
+            simulateRace();
+            printResult();
 
     }
     private static void createVehicles(){
+        racers.clear();
         racers.add(new Car("Cobra Exence"));
         racers.add(new Car("Vertex Blaze"));
         racers.add(new Car("Vindicator Freedom"));
@@ -35,21 +33,36 @@ public class Main {
         }
     }
     private static void simulateRace(){
-        boolean isRaining;
+
 
         for(int round = 0; round < 50; round++){
             //create a random number and set the isRaining variable true if the number smaller then 30 so is raining
             isRaining = rnd.nextInt(100)+ 1 <= 30;
-
-
             for(Racer racer: racers){
-                racer.moveForAnHour();
                 racer.setIsRaining(isRaining);
+                racer.moveForAnHour();
             }
         }
 
     }
-    void printResult(){
+    private static void printResult(){
+
+        Collections.sort(racers, new Comparator<Racer>() {
+            @Override
+            public int compare(Racer o1, Racer o2) {
+                return o2.getDistanceTraveled() - o1.getDistanceTraveled();
+            }
+        });
+        int counter = 1;
+        for(Racer racer: racers){
+            System.out.println(String.format(" %4s",counter+". ") + String.format("%-20s",racer.getName())
+                    + String.format("%-10s",Integer.toString(racer.getDistanceTraveled()) + " km")
+                    + racer.getClass().getSimpleName()
+
+            );
+            counter++;
+        }
+
 
     }
 }
